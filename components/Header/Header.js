590636a79aba20b74eebd90ea557c5b7caee659b
ashtken <ashleytkennedy10@gmail.com>
2021-03-22 16:47:28 +0000

    Added dark and light theme functionality

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,9 +1,15 @@
-import React from "react";
+import styles from "./Header.module.scss";
+import { useTheme } from "next-themes";
 
 export const Header = () => {
+	const { theme, setTheme } = useTheme();
+	const themeToggler = () => {
+		theme === "light" ? setTheme("dark") : setTheme("light");
+	};
 	return (
-		<div>
-			<h1>Header</h1>
+		<div className={styles.header}>
+			<h1>Where in the world?</h1>
+			<button onClick={themeToggler}>Switch Theme {theme}</button>
 		</div>
 	);
 };
